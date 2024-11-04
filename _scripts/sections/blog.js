af7da5fb0867e57ad66875a38d6c7a0c4aab6eff
Stefan Bowerman <stefbowerman@gmail.com>
2024-11-04 10:22:38 -0500

    Remove infiniteScroll. . Update taxi js version

--- _scripts/sections/blog.js
+++ _scripts/sections/blog.js
@@ -1,32 +1,9 @@
 import BaseSection from './base'
-import InfiniteScroll from '../core/infiniteScroll'
-
-const selectors = {
-  grid: '[data-grid]',
-  gridItem: '[data-grid-item]'
-}
 
 export default class BlogSection extends BaseSection {
   static TYPE = 'blog'
 
   constructor(container) {
     super(container)
-
-    this.$grid = $(selectors.grid, this.$container)
-
-    this.infiniteScroll = new InfiniteScroll(this.$container, {
-      selector: selectors.gridItem,
-      onFetchComplete: this.onFetchComplete.bind(this)
-    })
-  }
-
-  onUnload() {
-    this.infiniteScroll.destroy()
-
-    super.onUnload()    
-  }
-
-  onFetchComplete($selection) {
-    this.$grid.append($selection)
   }
 }
