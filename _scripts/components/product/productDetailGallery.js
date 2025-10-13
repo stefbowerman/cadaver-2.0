f698cb25a7b7fb72520bc9137721942d5cd2168d
Stefan Bowerman <stefbowerman@gmail.com>
2025-10-13 12:36:29 -0400

    POC - move product detail gallery to embla

--- _scripts/components/product/productDetailGallery.js
+++ _scripts/components/product/productDetailGallery.js
@@ -1,14 +1,67 @@
-import { Swiper } from 'swiper'
+import EmblaCarousel from 'embla-carousel'
 import BaseComponent from '@/components/base'
 
 const selectors = {
   slideshow: '[data-slideshow]',
-  slide: '.swiper-slide',
+  slide: '.embla__slide',
 }
 
 const classes = {
   isActive: 'is-active',
   slideshowDisabled: 'is-disabled'
+}
+
+const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
+  let dotNodes = []
+
+  const addDotBtnsWithClickHandlers = () => {
+    dotsNode.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => '<button class="embla__dot" type="button"></button>')
+      .join('')
+
+    const scrollTo = (index) => {
+      emblaApi.scrollTo(index)
+    }
+
+    dotNodes = Array.from(dotsNode.querySelectorAll('.embla__dot'))
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener('click', () => scrollTo(index), false)
+    })
+  }
+
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap()
+    const selected = emblaApi.selectedScrollSnap()
+    dotNodes[previous].classList.remove('embla__dot--selected')
+    dotNodes[selected].classList.add('embla__dot--selected')
+  }
+
+  emblaApi
+    .on('init', addDotBtnsWithClickHandlers)
+    .on('reInit', addDotBtnsWithClickHandlers)
+    .on('init', toggleDotBtnsActive)
+    .on('reInit', toggleDotBtnsActive)
+    .on('select', toggleDotBtnsActive)
+
+  return () => {
+    dotsNode.innerHTML = ''
+  }
+}
+
+const addPaginationHandlers = (emblaApi, paginationNode) => {
+  const updatePagination = () => {
+    paginationNode.innerHTML = `${emblaApi.selectedScrollSnap() + 1} / ${emblaApi.scrollSnapList().length}`
+  }
+
+  emblaApi
+    .on('init', updatePagination)
+    .on('reInit', updatePagination)
+    .on('select', updatePagination)
+
+  return () => {
+    paginationNode.innerHTML = ''
+  }
 }
 
 export default class ProductDetailGallery extends BaseComponent {
@@ -19,30 +72,41 @@ export default class ProductDetailGallery extends BaseComponent {
 
     this.images = this.qsa('img')
 
+    this.emblaNode = this.qs('.embla')
     this.slideshow = this.qs(selectors.slideshow)
 
-    this.slideCount = this.qsa(selectors.slide).length // Swiper can't give you the total count without duplicates.. Also don't set this to a variable since it changes as soon as swiper is initialized
+    this.slideCount = this.qsa(selectors.slide).length
     this.color = this.dataset.color
     this.isActive = this.el.classList.contains(classes.isActive)
 
-    this.swiper = new Swiper(this.slideshow, {     
-      init: false,
-      loop: this.slideCount > 1,
-      effect: 'slide',
-      speed: 600,
-      simulateTouch: true,
-      on: {
-        realIndexChange: (swiper) => {
-          // 
-        }
-      }       
-    }) 
-    
-    this.swiper.init()
+
+    this.emblaApi = EmblaCarousel(this.slideshow, {
+      loop: this.slideCount > 1
+    })
+
+    this.removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+      this.emblaApi,
+      this.emblaNode.querySelector('.embla__dots')
+    )
+
+    this.removePaginationHandlers = addPaginationHandlers(
+      this.emblaApi,
+      this.emblaNode.querySelector('.embla__pagination')
+    )
 
     if (this.slideCount === 1) {
       this.slideshow.classList.add(classes.slideshowDisabled)
+
+      // @TODO - Should remove the dots and pagination (or not initialize in the first place...)
     }    
+  }
+
+  destroy() {
+    this.removePaginationHandlers()
+    this.removeDotBtnsAndClickHandlers()
+    this.emblaApi.destroy()
+
+    super.destroy()
   }
 
   activate() {
